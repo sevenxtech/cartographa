aede2852b5d08323bde259e50eb029655a0af121
samueljd <samuelj.ernest@gmail.com>
2022-06-04 17:26:57 +0530

    reactquery

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (<QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+  </QueryClientProvider>)
 }
 
 export default MyApp;
---
+++ src/graphql/components/Map/Map.tsx
@@ -0,0 +1,46 @@
+import { ReactChildren, useEffect, ReactElement } from 'react';
+import L from 'leaflet';
+import * as ReactLeaflet from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Map as BaseMap } from 'leaflet';
+
+import styles from './Map.module.css';
+
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+export interface MapPropType {
+  children: (RL: typeof ReactLeaflet,  map: BaseMap) => ReactElement
+}
+
+
+const { MapContainer, MapConsumer } = ReactLeaflet;
+
+const Map = ({ children, ...rest }: MapPropType,className:string) => {
+  let mapClassName = styles.map;
+
+  if (className) {
+    mapClassName = `${mapClassName} ${className}`;
+  }
+
+  useEffect(() => {
+    (async function init() {
+      delete L.Icon.Default.prototype._getIconUrl;
+
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl: iconRetinaUrl.src,
+        iconUrl: iconUrl.src,
+        shadowUrl: shadowUrl.src,
+      });
+    })();
+  }, []);
+
+  return (
+    <MapContainer className={mapClassName} {...rest}>
+      {children(ReactLeaflet)}
+    </MapContainer>
+  )
+}
+
+export default Map;
