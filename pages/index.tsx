82ec8e2b6c11febc477ca9b10304a8be8c12a7d2
samueljd <samuelj.ernest@gmail.com>
2022-06-14 15:44:55 +0530

    build fail

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,48 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { LatLngBoundsExpression, LatLngExpression } from 'leaflet';
+import { GraphQLClient } from "graphql-request";
+import { GetStaticProps } from "next";
+import { GeoJsonObject } from 'geojson';
 
 import Map from '../src/components/Map';
-
-import { useGetCoordinatesQuery, useGetUsersQuery } from '../src/graphql/generated/graphql';
+import { GetCoordinatesDocument,GetCoordinatesQuery,GetOneCoordinatesQuery,GetOneCoordinatesDocument} from '../src/graphql/generated/graphql';
 import styles from "../styles/Home.module.css";
 
 const DEFAULT_CENTER: LatLngExpression = [28.70,77.10]
 
-const Home: NextPage = () => {
-  // const { data, isError, isFetching, isLoading, isFetched } = useGetCoordinatesQuery();
-  const { data, isError, isFetching, isLoading, isFetched } = useGetCoordinatesQuery();
+const hasuraUrl = process.env.HASURA_GRAPHQL_URL;
+const token = process.env.HASURA_GRAPHQL_ADMIN_SECRET;
+
+const client = new GraphQLClient(hasuraUrl as string, {
+  headers: {
+    "x-hasura-admin-secret" : token!,
+  },
+});
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  try {
+    const data: GetOneCoordinatesQuery = await client.request(GetOneCoordinatesDocument);
+    console.log(data);
+    return {
+      props: data,
+    };
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
+export interface GeoJSONPropType {
+  // children: (RL: typeof ReactLeaflet,  map: BaseMap) => ReactElement;
+  data?:string;
+  // center?:LatLngExpression;
+  // zoom?:number;
+
+}
+
+const Home: NextPage = (props) => {
+  
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -21,16 +52,17 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        {isFetched && <Map className={styles.homeMap} center={DEFAULT_CENTER} zoom={12}>
+        {<Map className={styles.homeMap} center={DEFAULT_CENTER} zoom={12}>
           {({ TileLayer, Marker, Popup, GeoJSON, Circle, Polygon, Polyline, Rectangle }) => (
             <>
               <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               />
-              <GeoJSON data={data?.postgis}>
-                {/* {data?.postgis[0].properties.language} */}
+              <GeoJSON data={props.postgis}>
+                {/* {props.postgis[0].properties.language} */}
               </GeoJSON>
+              <GeoJSON data={{ type: "Feature",geometry: {type: "Point",coordinates: [125.6, 10.1] }} as GeoJsonObject }/>
               <Marker position={DEFAULT_CENTER}>
                 <Popup>
                   A pretty CSS3 popup. <br /> Easily customizable.
@@ -38,8 +70,7 @@ const Home: NextPage = () => {
               </Marker>
             </>
           )}
-        </Map>
-        }
+        </Map>}
       </main>
     </div>
   );
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -3,19 +3,25 @@ import L from 'leaflet';
 import * as ReactLeaflet from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { Map as BaseMap } from 'leaflet';
+import { LatLngBoundsExpression, LatLngExpression } from 'leaflet';
 
 import styles from './Map.module.css';
 
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+import internal from 'stream';
 
 export interface MapPropType {
-  children: (RL: typeof ReactLeaflet,  map: BaseMap) => ReactElement
+  children: (RL: typeof ReactLeaflet,  map: BaseMap) => ReactElement;
+  className?:string;
+  center?:LatLngExpression;
+  zoom?:number;
+
 }
 
 
-const { MapContainer, MapConsumer } = ReactLeaflet;
+const { MapContainer } = ReactLeaflet;
 
 const Map = ({ children, ...rest }: MapPropType,className:string) => {
   let mapClassName = styles.map;
