edd9e4c73e4b36a4b5646d6cf8e41ccf57c29982
samueljd <samuelj.ernest@gmail.com>
2022-07-11 19:23:32 +0530

    code cleanup

--- src/components/Select/MapSelect.tsx
+++ src/components/Select/MapSelect.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useRef, useState, Fragment } from 'react';
 
 import styles from "@../../styles/Home.module.css";
-import { Disclosure, Menu, Transition, Combobox } from '@headlessui/react';
-import { BellIcon, CheckIcon, MenuIcon, SelectorIcon, XIcon } from '@heroicons/react/outline';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { BellIcon,  MenuIcon, XIcon } from '@heroicons/react/outline';
 import { LatLngExpression } from 'leaflet';
 
 import Map from '@components/Map';
 
 import simpleJson from '../../data/SimpleWorldLang.json'
+import SelectList from './SelectList';
 
 const languages: any[] = [];
 
@@ -43,8 +44,6 @@ const MapSelect = () => {
   const [mapData, setmapData] = useState<any>();
   const [isLoading, setLoading] = useState(false);
   const [selected, setSelected] = useState(languages[0]);
-  const [query, setQuery] = useState('')
-
   const geoJsonRef = useRef<any>();
 
   useEffect(() => {
@@ -58,13 +57,6 @@ const MapSelect = () => {
     setmapData(data)
     setLoading(false)
   }, [setSelected, selected])
-
-  const filteredLanguages =
-    query === ''
-      ? languages
-      : languages.filter((e) => {
-        return e.language.toLowerCase().includes(query.toLowerCase())
-      })
 
   useEffect(() => {
     if (geoJsonRef.current) {
@@ -109,71 +101,8 @@ const MapSelect = () => {
                         ))}
 
                         {/* Listbox */}
+                        <SelectList selectedprops={[selected,setSelected,languages]}/>
 
-                        <div className="w-72">
-                          <Combobox value={selected} onChange={setSelected}>
-                            <div className="relative mt-1">
-                              <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-                                <Combobox.Input
-                                  className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-                                  displayValue={(e:any) => e.language}
-                                  onChange={(event) => setQuery(event.target.value)}
-                                />
-                                <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                                  <SelectorIcon
-                                    className="h-5 w-5 text-gray-400"
-                                    aria-hidden="true"
-                                  />
-                                </Combobox.Button>
-                              </div>
-                              <Transition
-                                as={Fragment}
-                                leave="transition ease-in duration-100"
-                                leaveFrom="opacity-100"
-                                leaveTo="opacity-0"
-                                afterLeave={() => setQuery('')}
-                              >
-                                <Combobox.Options className=" z-20 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                  {filteredLanguages.length === 0 && query !== '' ? (
-                                    <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                                      Nothing found.
-                                    </div>
-                                  ) : (
-                                    filteredLanguages.map((e, idx) => (
-                                      <Combobox.Option
-                                        key={idx}
-                                        className={({ active }) =>
-                                          `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-teal-600 text-white' : 'text-gray-900'
-                                          }`
-                                        }
-                                        value={e}
-                                      >
-                                        {({ selected, active }) => (
-                                          <>
-                                            <span
-                                              className={`block truncate ${selected ? 'font-medium' : 'font-normal'
-                                                }`}
-                                            >
-                                              {e.language}
-                                            </span>
-                                            {selected ? (
-                                              <span
-                                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-teal-600'
-                                                  }`}
-                                              >
-                                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                              </span>
-                                            ) : null}
-                                          </>
-                                        )}
-                                      </Combobox.Option>
-                                    ))
-                                  )}
-                                </Combobox.Options>
-                              </Transition>
-                            </div>
-                          </Combobox>
-                        </div>
                       </div>
                     </div>
                   </div>
---
+++ src/components/Select/SelectList.tsx
@@ -0,0 +1,86 @@
+import { useState, Fragment } from 'react';
+
+import { Transition, Combobox } from '@headlessui/react';
+import {CheckIcon,SelectorIcon } from '@heroicons/react/outline';
+
+const SelectList = (props: any) => {
+  const [selectedList, setSelectedList, languages] = props.selectedprops;
+  const [query, setQuery] = useState('');
+
+  const filteredLanguages =
+    query === ''
+      ? languages
+      : languages.filter((e:any) => {
+        return e.language.toLowerCase().includes(query.toLowerCase())
+      })
+
+  return (
+
+    <div className="w-72">
+      <Combobox value={selectedList} onChange={setSelectedList}>
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+              displayValue={(e: any) => e.language}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <SelectorIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className=" z-20 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {filteredLanguages.length === 0 && query !== '' ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredLanguages.map((e:any, idx:any) => (
+                  <Combobox.Option
+                    key={idx}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                      }`
+                    }
+                    value={e}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                            }`}
+                        >
+                          {e.language}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-teal-600'
+                              }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  )
+}
+
+export default SelectList;
